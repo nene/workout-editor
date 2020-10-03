1056954efe0ae9f38e80993d3895976cc4abb64d
Rene Saarsoo <nene@triin.net>
2020-10-04 00:35:01 +0300

    Typo

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ Description: Try changing it, and see what happens below.
 Warmup: 10:00 30%..75%
 Interval: 15:00 100% 90rpm
   @ 00:00 Start off easy
-  @ 01:00 Settle into rythm
+  @ 01:00 Settle into rhythm
   @ 07:30 Half way through
   @ 14:00 Final minute, stay strong!
 Rest: 10:00 75%
