c3186c1023968b435e896264ba748c26e51a5930
Rene Saarsoo <nene@triin.net>
2020-10-04 15:27:49 +0300

    Updates

---
+++ precache-manifest.b000e4cd3662e7e684583b05821551ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b297e8789466cec4172c0075d76108b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1db5f53e610cf437f56c",
+    "url": "/static/css/main.e585a887.chunk.css"
+  },
+  {
+    "revision": "38ef632643d6427063df",
+    "url": "/static/js/2.19276c08.chunk.js"
+  },
+  {
+    "revision": "eab6cd95c8b7f7a43af593f2585901b3",
+    "url": "/static/js/2.19276c08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db5f53e610cf437f56c",
+    "url": "/static/js/main.05bc61d3.chunk.js"
+  },
+  {
+    "revision": "74230e22d66d184fd9d6",
+    "url": "/static/js/runtime-main.3e2a88f1.js"
+  }
+]);
